bfa6bcfe59d8a919c93172c36d5b42669b080f84
R0moo <roditto123@gmail.com>
2025-05-29 11:36:23 -0300

    Breeze, mailhog e listagem de users

--- resources/views/autentica/index.blade.php
+++ resources/views/autentica/index.blade.php
@@ -1,6 +1,13 @@
 <h1>Usuários</h1>
 <hr>
-
+@if (Auth::user())
+    <b>Olá {{ Auth::user()->name }}.</b><br>
+    <a href="{{ route('autentica.logout') }}">Sair</a>
+@else
+    Você não está autenticado ;-; <br>
+    <a href="{{ route('autentica.login') }}">Entrar</a>
+@endif
+<hr>
 @if ($errors->any())
 <div style="color:red">
 <h3>ERRO PORRA</h3>
@@ -24,3 +31,10 @@
 <br>
 <input type="submit" value="gravar">
 </form>
+<hr>
+
+<ul>
+@foreach ($usuarios as $usuario)
+    <li>{{ $usuario->name }}</li>
+@endforeach
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,20 @@
 <?php
 
-use App\Http\Controllers\AutenticaController;
-use App\Http\Controllers\CalculosController;
-use App\Http\Controllers\KeepinhoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/teste', function () {
-    return view('teste');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/teste/{valor}', function ($valor) {
-    return "Você digitou: {$valor}";
-});
-
-//Calculos
-Route::get("/calc/somar/{x}/{y}", [CalculosController::class, "somar"]);
-Route::get("/calc/sub/{x}/{y}", [CalculosController::class, "subtrair"]);
-Route::get("/calc//{x}/{y}", [CalculosController::class, "multiplicar"]);
-Route::get("/calc/sub/{x}/{y}", [CalculosController::class, "dividir"]);
-Route::get("/calc/quad/{x}", [CalculosController::class, "quadrado"]);
-
-//Rotas do keepinho
-Route::prefix("/keep")->group(function(){
-    Route::get('/', [KeepinhoController::class,'index'])->name('keep');
-    
-    Route::post('/gravar', [KeepinhoController::class,'gravar'])->name('keep.gravar');
-
-    Route::get('/editar/{nota}', [KeepinhoController::class,'editar'])->name('keep.editar'); //Forms
-
-    Route::put('/editar', [KeepinhoController::class,'editar'])->name('keep.editarGravar'); //Ação
-
-    Route::delete('/apagar/{nota}', [KeepinhoController::class,'apagar'])->name('keep.apagar');
-
-    Route::get('/lixeira', [KeepinhoController::class,'lixeira'])->name('keep.lixeira');
-
-    Route::get('/restaurar/{nota}', [KeepinhoController::class, 'restaurar'])->name('keep.restaurar');
-
-});
-
-    Route::get('/autenticar', [AutenticaController::class, 'index'])->name('autentica');
-
-    Route::post('/autenticar/gravar', [AutenticaController::class, 'gravar'])->name('autentica.gravar');
-
-    Route::get('/autenticar/login', [AutenticaController::class, 'login'])->name('autentica.login');
-    Route::post('/autenticar/login', [AutenticaController::class, 'login']);
+require __DIR__.'/auth.php';
